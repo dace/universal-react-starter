e04a3fa66a9aa4b45616bd682231a0d4c017e10b
Dace Willmott <willmott@brooklynunited.com>
2015-11-02 17:49:55 -0500

    initial commit

---
+++ app/shared/components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello,7897987879 World!</h1>
+      </div>
+    );
+  }
+}
